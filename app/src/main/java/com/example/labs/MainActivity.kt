3b231657f6e519f7d2daf54237c3dbcf4d044fc9
Михаил Соколов <sokol.m00@gmail.com>
2023-06-09 13:55:48 +0700

    First Generated repository

---
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.labs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.labs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bdCl : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bdCl = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bdCl.root)
+        //bdCl.menuTitle.text = "Hello, Android!" - проверка работоспособности Binding Class
+    }
+    fun onClickL1 (view : View) {
+        Log.i("MyLog", "Button 1 pressed")
+    }
+
+    fun onClickL2(view: View) {
+        Log.i("MyLog", "Button 2 pressed")
+    }
+    fun onClickL3(view: View) {
+        Log.i("MyLog", "Button 3 pressed")
+    }
+    fun onClickL4(view: View) {
+        Log.i("MyLog", "Button 4 pressed")
+    }
+    fun onClickL5(view: View) {
+        Log.i("MyLog", "Button 5 pressed")
+    }
+    fun onClickL6(view: View) {
+        Log.i("MyLog", "Button 6 pressed")
+    }
+    fun onClickL7(view: View) {
+        Log.i("MyLog", "Button 7 pressed")
+    }
+    fun onClickL8(view: View) {
+        Log.i("MyLog", "Button 8 pressed")
+    }
+}
